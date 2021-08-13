bc161a7557f1bdca3f3ec57f5b3bc6dc8a59e1b4
Jekyll <kalugin@outlook.com>
2021-08-13 23:52:48 +0300

    Add project files.

---
+++ CacheSync/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace FormSync
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_formFolder = new System.Windows.Forms.Button();
+            this.textBox_formFolder = new System.Windows.Forms.TextBox();
+            this.checkBox_enabled = new System.Windows.Forms.CheckBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button_deployFolder = new System.Windows.Forms.Button();
+            this.textBox_deployFolder = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox_cacheFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_formFolder
+            // 
+            this.button_formFolder.Location = new System.Drawing.Point(12, 36);
+            this.button_formFolder.Name = "button_formFolder";
+            this.button_formFolder.Size = new System.Drawing.Size(92, 23);
+            this.button_formFolder.TabIndex = 0;
+            this.button_formFolder.Text = "Form folder";
+            this.button_formFolder.UseVisualStyleBackColor = true;
+            this.button_formFolder.Click += new System.EventHandler(this.Button_formFolder_Click);
+            // 
+            // textBox_formFolder
+            // 
+            this.textBox_formFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_formFolder.Location = new System.Drawing.Point(110, 37);
+            this.textBox_formFolder.Name = "textBox_formFolder";
+            this.textBox_formFolder.Size = new System.Drawing.Size(318, 23);
+            this.textBox_formFolder.TabIndex = 1;
+            this.textBox_formFolder.Leave += new System.EventHandler(this.TextBox_formFolder_Leave);
+            // 
+            // checkBox_enabled
+            // 
+            this.checkBox_enabled.AutoSize = true;
+            this.checkBox_enabled.Enabled = false;
+            this.checkBox_enabled.Location = new System.Drawing.Point(12, 12);
+            this.checkBox_enabled.Name = "checkBox_enabled";
+            this.checkBox_enabled.Size = new System.Drawing.Size(68, 19);
+            this.checkBox_enabled.TabIndex = 2;
+            this.checkBox_enabled.Text = "Enabled";
+            this.checkBox_enabled.UseVisualStyleBackColor = true;
+            this.checkBox_enabled.CheckedChanged += new System.EventHandler(this.CheckBox_enabled_CheckedChanged);
+            // 
+            // button_deployFolder
+            // 
+            this.button_deployFolder.Location = new System.Drawing.Point(12, 65);
+            this.button_deployFolder.Name = "button_deployFolder";
+            this.button_deployFolder.Size = new System.Drawing.Size(92, 23);
+            this.button_deployFolder.TabIndex = 0;
+            this.button_deployFolder.Text = "Deploy folder";
+            this.button_deployFolder.UseVisualStyleBackColor = true;
+            this.button_deployFolder.Click += new System.EventHandler(this.Button_deployFolder_Click);
+            // 
+            // textBox_deployFolder
+            // 
+            this.textBox_deployFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_deployFolder.Location = new System.Drawing.Point(110, 66);
+            this.textBox_deployFolder.Name = "textBox_deployFolder";
+            this.textBox_deployFolder.Size = new System.Drawing.Size(318, 23);
+            this.textBox_deployFolder.TabIndex = 1;
+            this.textBox_deployFolder.Leave += new System.EventHandler(this.TextBox_deployFolder_Leave);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Inactive";
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.NotifyIcon1_DoubleClick);
+            // 
+            // textBox_cacheFolder
+            // 
+            this.textBox_cacheFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_cacheFolder.Location = new System.Drawing.Point(110, 95);
+            this.textBox_cacheFolder.Name = "textBox_cacheFolder";
+            this.textBox_cacheFolder.ReadOnly = true;
+            this.textBox_cacheFolder.Size = new System.Drawing.Size(318, 23);
+            this.textBox_cacheFolder.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cache folder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 130);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox_enabled);
+            this.Controls.Add(this.textBox_cacheFolder);
+            this.Controls.Add(this.textBox_deployFolder);
+            this.Controls.Add(this.textBox_formFolder);
+            this.Controls.Add(this.button_deployFolder);
+            this.Controls.Add(this.button_formFolder);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CacheSync";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_formFolder;
+        private System.Windows.Forms.TextBox textBox_formFolder;
+        private System.Windows.Forms.CheckBox checkBox_enabled;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button_deployFolder;
+        private System.Windows.Forms.TextBox textBox_deployFolder;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox_cacheFolder;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CacheSync/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+using FormSync.Properties;
+
+namespace FormSync
+{
+    public partial class Form1 : Form
+    {
+        private string _formFolder = "";
+        private string _cacheFolder = "";
+        private string _deployFolder = "";
+        readonly private string _trackFiles;
+        readonly private string[] _clearFiles;
+        readonly private string _cacheFolderName;
+        private FileSystemWatcher _watcher;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_formFolder.Text = _formFolder = Settings.Default.FormFolder;
+            textBox_deployFolder.Text = _deployFolder = Settings.Default.DeployFolder;
+            textBox_cacheFolder.Text = _cacheFolder = Settings.Default.CacheFolder;
+            _trackFiles = Settings.Default.TrackFiles;
+            _clearFiles = Settings.Default.ClearFiles.Split(';', StringSplitOptions.RemoveEmptyEntries);
+            _cacheFolderName = Settings.Default.CacheFolderName;
+            CheckAllDirectoryExists();
+        }
+
+        private void Button_formFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = _formFolder;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK &&
+                !string.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+            {
+                _formFolder = folderBrowserDialog1.SelectedPath;
+                textBox_formFolder.Text = _formFolder;
+            }
+
+            CheckAllDirectoryExists();
+        }
+
+        private void Button_deployFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = _deployFolder;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK &&
+                !string.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+            {
+                _deployFolder = folderBrowserDialog1.SelectedPath;
+                textBox_deployFolder.Text = _deployFolder;
+
+                var newCachePath = GetCachePath(_deployFolder);
+                _cacheFolder = newCachePath;
+                textBox_cacheFolder.Text = _cacheFolder;
+            }
+
+            CheckAllDirectoryExists();
+        }
+
+        private void CheckBox_enabled_CheckedChanged(object sender, EventArgs e)
+        {
+            var en = checkBox_enabled.Checked;
+            WatcherStart(en);
+            button_formFolder.Enabled = !en;
+            button_deployFolder.Enabled = !en;
+            textBox_formFolder.Enabled = !en;
+            textBox_deployFolder.Enabled = !en;
+            textBox_cacheFolder.Enabled = !en;
+        }
+
+        private bool WatcherStart(bool en)
+        {
+            if (_watcher != null)
+            {
+                _watcher.Dispose();
+            }
+
+            if (!en)
+            {
+                notifyIcon1.Text = "Inactive";
+                return true;
+            }
+
+            notifyIcon1.Text = "Active";
+            try
+            {
+                _watcher = new FileSystemWatcher(_formFolder);
+
+                _watcher.NotifyFilter = NotifyFilters.DirectoryName
+                                        | NotifyFilters.FileName
+                                        | NotifyFilters.LastWrite
+                                        | NotifyFilters.Size;
+
+                _watcher.Changed += SyncCode;
+                _watcher.Created += SyncCode;
+                _watcher.Deleted += SyncCode;
+                _watcher.Renamed += SyncCode;
+                _watcher.Error += WatcherError;
+                _watcher.InternalBufferSize *= 10;
+
+                _watcher.Filter = _trackFiles;
+                _watcher.IncludeSubdirectories = true;
+                _watcher.EnableRaisingEvents = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can't start watcher: " + ex);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void TextBox_formFolder_Leave(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox_formFolder.Text))
+            {
+                _formFolder = textBox_formFolder.Text;
+            }
+            else
+            {
+                textBox_formFolder.Text = _formFolder;
+            }
+
+            CheckAllDirectoryExists();
+        }
+
+        private void TextBox_deployFolder_Leave(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox_deployFolder.Text))
+            {
+                _deployFolder = textBox_deployFolder.Text;
+            }
+            else
+            {
+                textBox_deployFolder.Text = _deployFolder;
+            }
+
+            var newCachePath = GetCachePath(_deployFolder);
+            _cacheFolder = newCachePath;
+            textBox_cacheFolder.Text = _cacheFolder;
+
+            CheckAllDirectoryExists();
+        }
+
+        private bool CheckAllDirectoryExists()
+        {
+            var result = Directory.Exists(_formFolder) && Directory.Exists(_deployFolder) &&
+                         !_formFolder.Contains(_deployFolder);
+            checkBox_enabled.Enabled = result;
+            if (!result)
+            {
+                checkBox_enabled.Checked = false;
+            }
+
+            return result;
+        }
+
+        private void SyncCode(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(_cacheFolder))
+                {
+                    foreach (var file in _clearFiles)
+                    {
+                        foreach (var f in Directory.EnumerateFiles(_cacheFolder, file))
+                        {
+                            File.Delete(f);
+                        }
+                    }
+                }
+
+                DirectoryCopy(_formFolder, _deployFolder, _trackFiles);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can't process files: " + ex);
+            }
+        }
+
+        private static void DirectoryCopy(string sourceDir, string destinationDir, string fileMask)
+        {
+            var filesToCopy = Directory.GetFiles(sourceDir, fileMask, SearchOption.AllDirectories);
+
+            foreach (var file in filesToCopy)
+            {
+                var fileName = file.Substring(sourceDir.Length);
+                var directoryPath = Path.Combine(destinationDir, Path.GetDirectoryName(fileName.Trim('\\')));
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                File.Copy(file, Path.Combine(directoryPath, Path.GetFileName(file)), true);
+            }
+        }
+
+        private static void WatcherError(object sender, ErrorEventArgs ex)
+        {
+            MessageBox.Show("Error watching files: " + ex);
+        }
+
+        private string GetCachePath(string dirPath)
+        {
+            return dirPath + "\\" + _cacheFolderName.Trim('\\') + "\\";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            WatcherStart(false);
+            Settings.Default.FormFolder = _formFolder;
+            Settings.Default.DeployFolder = _deployFolder;
+            Settings.Default.CacheFolder = _cacheFolder;
+            Settings.Default.Save();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void NotifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+    }
+}
